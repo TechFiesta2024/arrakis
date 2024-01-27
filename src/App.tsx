01a872017e0299575dc8ab8527d883dadd4849c1
bishakhne0gi <bneogi102002@gmail.com>
2024-01-27 19:24:25 +0530

    feat: fresh

--- src/App.tsx
+++ src/App.tsx
@@ -3,35 +3,10 @@ import './App.css'
 
 const App = () => {
   return (
-<<<<<<< HEAD
-    <div>Lessgo Lavdos</div>
-=======
+ 
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-        <h1>
-          test1
-        </h1>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+       <div>Lessgo Lavdos</div>
     </>
->>>>>>> 6418670b34ec36ef6f3686d8e6a530d81e190647
   )
 }
 
