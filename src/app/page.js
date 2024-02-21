a616027dbe4aa100937aeec02acf6a1dfd85add4
bishakhne0gi <bneogi102002@gmail.com>
2024-02-22 01:35:41 +0530

    feat: fav.ico

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,9 @@
 'use client'
 
 import HomeScreen from "@/app/home/index.jsx";
-import Head from "next/head";
-
 export default function Home() {
   return (
     <>
-
       <HomeScreen />
     </>
   );
