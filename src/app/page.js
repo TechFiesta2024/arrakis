a6e54e5d6a792490d35625c50a4a2e40d1eae1c6
bishakhne0gi <bneogi102002@gmail.com>
2024-02-21 20:56:35 +0530

    feat: added favicon.ico

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,13 @@
 'use client'
 
 import HomeScreen from "@/app/home/index.jsx";
+import Head from "next/head";
 
 export default function Home() {
   return (
-    <HomeScreen/>
+    <>
+
+      <HomeScreen />
+    </>
   );
 }
