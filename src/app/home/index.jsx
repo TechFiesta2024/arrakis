1eae6bf60c25d1568085fae4d206e3003451a044
r3yc0n1c <raja.majumdar.01@gmail.com>
2024-02-23 02:15:07 +0530

    feat: DragBox with matter.js

--- src/app/home/index.jsx
+++ src/app/home/index.jsx
@@ -1,11 +1,15 @@
-'use client'
+"use client";
+
+import DragBox from "@/components/Hero/DragBox";
 
 export default function HomeScreen() {
-    return (
-        <>
-            <div className='flex justify-center items-center py-40'>
-                <h1 className='text-xl text-red-600 '>From Home Page</h1>
-            </div>
-        </>
-    )
+  return (
+    <>
+      <div className="flex flex-col justify-center items-center py-40">
+        <h1 className="text-xl text-red-600 ">From Home Page</h1>
+        <DragBox />
+      </div>
+    </>
+  );
 }
+
