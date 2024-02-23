8d808def9ea1b94e82c020578c5a21ce20a04361
Chirantan Das <daschirantan0002@gmail.com>
2024-02-23 20:17:09 +0530

    Update Herosection component styling

--- src/components/Home/Herosection.jsx
+++ src/components/Home/Herosection.jsx
@@ -3,33 +3,33 @@
 const Herosection = () => {
   return (
       <>
-          <div className="w-full md:h-[800px] px-20 bg-white">
-              <div className="row-1 grid md:grid-cols-3 ">
-                  <div className="col-span-1 border ">
+          <div className="w-full h-screen px-20 ">
+              <div className="row-1 grid md:grid-cols-3 h-[30%]">
+                  <div className="col-span-1 border text-yellowish">
                       <h1>participate</h1>
                   </div>
-                  <div className="col-span-1 border">
+                  <div className="col-span-1 border text-yellowish">
                         <h1>floating button</h1>
                   </div>
-                  <div className="col-span-1 border">
+                  <div className="col-span-1 border text-yellowish">
                       <h1>
                           prizepool
                       </h1>
                   </div>
               </div>
-              <div className="row-2 grid md:grid-cols-1 ">
-                  <div className="col-span-1 border">
+              <div className="row-2 grid md:grid-cols-1 h-[40%]">
+                  <div className="col-span-1 border text-yellowish">
                       <h1>image</h1>
                   </div>
               </div>
-              <div className="row-3 grid md:grid-cols-3 ">
-                  <div className="col-span-1 border">
+              <div className="row-3 grid md:grid-cols-3 h-[30%]">
+                  <div className="col-span-1 border text-yellowish">
                         <h1>prizepool</h1>
                   </div>
-                  <div className="col-span-1 border">
+                  <div className="col-span-1 border text-yellowish">
                         <h1>prizepool</h1>
                   </div>
-                  <div className="col-span-1 border">
+                  <div className="col-span-1 border text-yellowish">
                         <h1>prizepool</h1>
                   </div>
               </div>
