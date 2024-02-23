9895e8254d741d4be37513d173871594c816427a
Swastik Bose <98341839+swastik-bose007@users.noreply.github.com>
2024-02-23 22:44:58 +0530

    Marqque structured

--- src/components/Home/Marquee.jsx
+++ src/components/Home/Marquee.jsx
@@ -1,9 +1,13 @@
 
 const Marquee = () => {
   return (
-      <>
+    <>
+      <div className="border-b-[0.5px] text-yellowish">
+        <div className="w-full px-20 py-8 my-20 text-yellowish border-[0.5px]">
           <div>Marquee</div>
-      </>
+        </div>
+      </div>
+    </>
   )
 }
 
