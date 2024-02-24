aabb6ca32bfb07c07f52530e71a17ed41eac7bc2
Chirantan Das <daschirantan0002@gmail.com>
2024-02-24 18:06:40 +0530

    Team Page done with responsiveness

--- public/assets/index.js
+++ public/assets/index.js
@@ -3,13 +3,17 @@ import LogoWhatsapp from './whatsapp.svg'
 import LogoLinkedIn from './linkedin.svg'
 import LogoX from './Vector.svg'
 import LogoInstagram from './instagram.svg'
+import LogoLinkedInBlack from './logoLinkedinBlack.svg'
+import LogoXBlack from './logoXblack.svg'
 
 const Images = {
     logoAot: LogoAot,
     logoWhatsapp: LogoWhatsapp,
     logoLinkedIn: LogoLinkedIn,
     logoX: LogoX,
-    logoInstagram: LogoInstagram
+    logoInstagram: LogoInstagram,
+    logoLinkedInBlack: LogoLinkedInBlack,
+    logoXBlack: LogoXBlack
 };
 
 export default Images;
---
+++ src/components/Team/Card.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import TeamCard from './TeamCard'
+
+const Card = ({ filteredData }) => {
+  return (
+      <>
+          <div className='grid lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1'>
+              {filteredData.map((member, index) => (
+                  <TeamCard key={index} member={member} />
+              ))}
+          </div>
+      </>
+  )
+}
+
+export default Card
+
+// mapping of TeamCard component
