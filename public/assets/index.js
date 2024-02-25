f8cb1aa9b097d0151475aeca33c97fb95dfb0111
Shanit Paul <shanit.kumarpaul.20@aot.edu.in>
2024-02-25 12:59:34 +0530

    dashboard workshop

--- public/assets/index.js
+++ public/assets/index.js
@@ -3,13 +3,33 @@ import LogoWhatsapp from './whatsapp.svg'
 import LogoLinkedIn from './linkedin.svg'
 import LogoX from './Vector.svg'
 import LogoInstagram from './instagram.svg'
+import IconRectangle from './rectangle.svg'
+import IconStar from './star.svg'
+import IconScreenMirror from './screenmirror.svg'
+import IconArrowRight from './arrowright.svg'
+import IconCpu from './cpu.svg'
+import IconMonitorMobile from './monitormobile.svg'
+import IconLink2 from './link2.svg'
+import IconGlobal from './global.svg'
+import IconClock from './clock.svg'
+import IconCalendar from './calendar.svg'
 
 const Images = {
     logoAot: LogoAot,
     logoWhatsapp: LogoWhatsapp,
     logoLinkedIn: LogoLinkedIn,
     logoX: LogoX,
-    logoInstagram: LogoInstagram
+    logoInstagram: LogoInstagram,
+    iconStar: IconStar,
+    iconRectangle: IconRectangle,
+    iconScreenMirror: IconScreenMirror,
+    iconArrowRight: IconArrowRight,
+    iconCpu: IconCpu,
+    iconMonitorMobile: IconMonitorMobile,
+    iconLink2: IconLink2,
+    iconClock: IconClock,
+    iconCalendar:IconCalendar,
+    iconGlobal: IconGlobal,
 };
 
 export default Images;
