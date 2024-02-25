6446d4593f8f3b6b078615e9acf15b3a4ce2f20e
Swastik Bose <98341839+swastik-bose007@users.noreply.github.com>
2024-02-25 23:24:34 +0530

    text alignment

--- src/components/Home/UnleashSkills.jsx
+++ src/components/Home/UnleashSkills.jsx
@@ -11,7 +11,7 @@ const UnleashSkills = () => {
       <div className="unleashskills__mainContainer border-y-[0.5px] border-yellowish  ">
         <div className=" uleashskills__container items-center justify-center w-full h-[428px] md:px-20">
           <div className="unleashtext__container flex justify-center items-center flex-col border-x-[0.5px] text-yellowish  h-[100%]  overflow-hidden">
-            <h1 className='font-anton pb-4 text-[60px] md:text-[180px]'>Unleash Skills</h1>
+            <h1 className='font-anton pb-4 text-[60px] md:text-[100px] lg:text-[180px] text-center'>Unleash Skills</h1>
             <Link href='/bootcamp' className={` ${flexStylesCenter} bg-red px-10 flex z-20 text-white rounded-[8px] gap-x-2 transition-transform hover:scale-x-110`}>
               <div className="buttoncontent flex py-2">
                 Check Workshops
