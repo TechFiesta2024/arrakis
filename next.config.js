f88e4d7d00a177873d4d4dfa1f608f6c634bb84d
ABHIRUP <abhirupsaha02@gmail.com>
2024-03-13 20:15:58 +0530

    trying caching

--- next.config.js
+++ next.config.js
@@ -3,6 +3,10 @@ const nextConfig = {
     reactStrictMode: false,
     images: {
         domains: ['lh3.googleusercontent.com'],
+    },
+    headers: {
+        key: 'Cache-Control',
+        value: 's-maxage=1, stale-while-revalidate=59'
     }
 };
 
