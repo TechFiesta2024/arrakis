dcc598a68386ad4a7356325b8ee3be4758629401
ABHIRUP <abhirupsaha02@gmail.com>
2024-03-14 02:48:52 +0530

    loader added

--- src/app/workshop/[id]/page.jsx
+++ src/app/workshop/[id]/page.jsx
@@ -1,7 +1,10 @@
 import dynamic from "next/dynamic";
 import workshops from '/public/data/workshop.json';
+import Preloader from "@/components/Global/Preloader";
 
-const EventWorkshopInfo = dynamic(() => import('@/components/Global/Info'))
+const EventWorkshopInfo = dynamic(() => import('@/components/Global/Info'), {
+	loading: () => <Preloader width="8rem" height="8rem" color="red" />,
+})
 
 export default function WorkshopPageById({ params }) {
 	return (
