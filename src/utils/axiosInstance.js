24c0b68774c958722926a907e9df24c6ae536603
ABHIRUP <abhirupsaha02@gmail.com>
2024-03-15 20:28:02 +0530

    fix env

--- src/utils/axiosInstance.js
+++ src/utils/axiosInstance.js
@@ -1,7 +1,7 @@
 const { default: axios } = require("axios");
 
 const axiosInstance = axios.create({
-	baseURL: process.env.NEXT_PUBLIC_MESSIAH_DEV_URL,
+	baseURL: process.env.NEXT_PUBLIC_MESSIAH_URL,
 	headers: {
 		"Access-Control-Allow-Headers": "*",
 	},
