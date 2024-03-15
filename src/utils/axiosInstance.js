6aec52d8d661f233c72f277155770a7a105a359d
ABHIRUP <abhirupsaha02@gmail.com>
2024-03-15 19:58:59 +0530

    .

--- src/utils/axiosInstance.js
+++ src/utils/axiosInstance.js
@@ -1,6 +1,6 @@
 const { default: axios } = require("axios");
 
-const environments = ['development', 'preview']
+const environments = ['development', 'Preview']
 
 const axiosInstance = axios.create({
 	baseURL: `${environments.includes(process.env.NODE_ENV) ? process.env.NEXT_PUBLIC_MESSIAH_DEV_URL : process.env.NEXT_PUBLIC_MESSIAH_PROD_URL}`,
