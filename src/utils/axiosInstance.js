20847d268672e34e9378031bc768689bfd9e56e1
ABHIRUP <abhirupsaha02@gmail.com>
2024-03-15 19:55:40 +0530

    env update

--- src/utils/axiosInstance.js
+++ src/utils/axiosInstance.js
@@ -1,7 +1,9 @@
 const { default: axios } = require("axios");
 
+const environments = ['development', 'preview']
+
 const axiosInstance = axios.create({
-	baseURL: `${process.env.NODE_ENV === 'development' ? process.env.NEXT_PUBLIC_MESSIAH_DEV_URL : process.env.NEXT_PUBLIC_MESSIAH_PROD_URL}`,
+	baseURL: `${environments.includes(process.env.NODE_ENV) ? process.env.NEXT_PUBLIC_MESSIAH_DEV_URL : process.env.NEXT_PUBLIC_MESSIAH_PROD_URL}`,
 	headers: {
 		"Access-Control-Allow-Headers": "*",
 	},
