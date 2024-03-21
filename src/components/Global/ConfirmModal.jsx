5be70ca66ef83d3fcb7ff40c1cde0812a0928d4f
Chirantan Das <82677314+CHIRANTAN-001@users.noreply.github.com>
2024-03-21 22:54:51 +0530

    Update ConfirmModal.jsx

--- src/components/Global/ConfirmModal.jsx
+++ src/components/Global/ConfirmModal.jsx
@@ -37,7 +37,7 @@ export default function ConfirmModal({ children, description }) {
 					<div className="bg-yellowish flex flex-col justify-center items-center gap-4 py-5 px-8 rounded-lg h-full">
 						<Image src={Images.warning} className="h-12 w-12" />
 						<p className="text-black text-xl md:text-3xl font-generalsans-semibold">{description}</p>
-						<span className="text-center text-black text-sm md:text-md">Once you regester you can not delete the team</span>
+{/* 						<span className="text-center text-black text-sm md:text-md">Once you regester you can not delete the team</span> */}
 						<div className="flex justify-evenly items-center w-full gap-3 text-white place-content-end mt-6">
 							<button className="bg-red py-1 px-2 rounded-md w-full font-generalsans" onClick={hide}>Cancel</button>
 							<button className="rounded-md text-black w-full font-generalsans" onClick={confirm}>Confirm</button>
